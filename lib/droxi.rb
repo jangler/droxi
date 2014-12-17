2104329a5a26b6d6407b3c6f89b3d4c4182decab
Brandon Mulcahy <brandon@jangler.info>
2014-12-17 16:15:06 -0500

    update metadata for v0.2.3

--- lib/droxi.rb
+++ lib/droxi.rb
@@ -10,7 +10,7 @@ require_relative 'droxi/text'
 # Command-line Dropbox client module.
 module Droxi
   # Version number of the program.
-  VERSION = '0.2.2'
+  VERSION = '0.2.3'
 
   # Message to display when invoked with the --help option.
   HELP_TEXT =
